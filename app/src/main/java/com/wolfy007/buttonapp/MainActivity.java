be4f9bb059553f12d15ee21d31004421cd7e8b79
wolfy007 <bhaskar15435@gmail.com>
2019-06-17 12:17:12 +0530

    project is completed

---
+++ app/src/main/java/com/wolfy007/buttonapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.wolfy007.buttonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+
+        EditText myTextField = (EditText) findViewById(R.id.textfield);
+        Log.i("info", myTextField.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
